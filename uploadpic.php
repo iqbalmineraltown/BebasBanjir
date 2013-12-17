73309953b33f9ba62b66a35b31ed28fdadb2c9eb
Muhammad Iqbal <muhammad.iqbal018@ui.ac.id>
2013-12-17 14:03:57 +0700

    bikin file dbconnect biar gak berulang-ulang buka db

--- uploadpic.php
+++ uploadpic.php
@@ -11,15 +11,8 @@
 </form>
 
 <?php
-
-$server = "localhost";
-$username = "root";
-$password = "";
-$database = "siagabanjir";
-
-mysql_connect($server, $username, $password) or die("Koneksi gagal");
-mysql_select_db($database) or die("Database tidak bisa dibuka");
-mysql_select_db("siagabanjir");
+//connect database
+require_once('dbconnect.php');
 
 if(isset($_POST['upload']) && $_FILES['userfile']['size'] > 0)
 {
